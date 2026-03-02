a03465b606a4b352a2590a89abc8e936d7347b05
victoboy <victoboy876@gmail.com>
2026-03-02 19:11:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -98,5 +98,8 @@ def logout():
     session.pop("user", None)
     return redirect("/")
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
